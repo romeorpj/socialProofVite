f2dbd12d14617499dcf72f3c9eab469762b09f5f
Richard <74265663+romeorpj@users.noreply.github.com>
2024-02-07 15:28:48 -0600

    adding main sections

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        {/* INSERT TOP SECTION */}
+        <section className="bottom-section">
+
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TopSection.jsx
@@ -0,0 +1,17 @@
+
+
+const TopSection = () => {
+	return (
+		<>
+			<section className='top-section'>
+				<section className="first-section">
+				</section>
+				<section className="second-section">
+				</section>
+			</section>
+		</>
+	)
+}
+
+export default TopSection
+TopSection.displayName = "TopSection"
